703f0fa024acb861c5840a2160edbdf05541cc0b
Eric Chin <exc@E14MBP.socal.rr.com>
2022-06-12 18:48:27 -0700

    Created Hello component

--- 01. Introduction to React/part1/src/App.js
+++ 01. Introduction to React/part1/src/App.js
@@ -1,14 +1,16 @@
-const App = () => {
-    const now = new Date();
-    const a = 10;
-    const b = 20;
-
+const Hello = () => {
     return (
         <div>
-            <p>Hello world, it is {now.toString()}</p>
-            <p>
-                {a} plus {b} is {a + b}
-            </p>
+            <p>Hello world</p>
+        </div>
+    )
+};
+
+const App = () => {
+    return (
+        <div>
+            <p>Greetings</p>
+            <Hello />
         </div>
     );
 };
