85681592da4550b895bc8eddb77ad2a0ea5bc83b
Eric Chin <exc@E14MBP.socal.rr.com>
2022-06-12 18:50:58 -0700

    Passing multiple props

--- 01. Introduction to React/part1/src/App.js
+++ 01. Introduction to React/part1/src/App.js
@@ -1,17 +1,20 @@
 const Hello = (props) => {
     return (
         <div>
-            <p>Hello {props.name}</p>
+            <p>Hello {props.name}, you are {props.age} years old</p>
         </div>
     )
 };
 
 const App = () => {
+    const name = "Peter";
+    const age = 10;
+
     return (
         <div>
             <p>Greetings</p>
-            <Hello name="George" />
-            <Hello name="Daisy" />
+            <Hello name="Maya" age={26 + 10} />
+            <Hello name={name} age={age} />
         </div>
     );
 };
