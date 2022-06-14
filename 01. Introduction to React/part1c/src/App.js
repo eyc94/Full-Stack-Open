ae4f6d1f924c08a5d5cc011d75b0beb3fa925b0d
Eric Chin <ericychin94@gmail.com>
2022-06-14 02:28:07 -0700

    Destructured Hello further

--- 01. Introduction to React/part1c/src/App.js
+++ 01. Introduction to React/part1c/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 
-const Hello = (props) => {
-    const name = props.name;
-    const age = props.age;
+const Hello = ({ name, age }) => {
 
     const bornYear = () => new Date().getFullYear() - age;
 
