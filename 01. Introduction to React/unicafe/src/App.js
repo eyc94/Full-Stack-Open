429a853ecc311fd257ae19feee4a1cf5595c8cb1
Eric Chin <ericychin94@gmail.com>
2022-06-16 03:22:21 -0700

    Readded comment

--- 01. Introduction to React/unicafe/src/App.js
+++ 01. Introduction to React/unicafe/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 const App = () => {
+    // Save clicks of each button to its own state.
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
