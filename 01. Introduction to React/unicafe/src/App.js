e0e8e3ae8d8bb135572505eb2dc8484b62b5afd1
Eric Chin <ericychin94@gmail.com>
2022-06-16 18:51:13 -0700

    Removed comment

--- 01. Introduction to React/unicafe/src/App.js
+++ 01. Introduction to React/unicafe/src/App.js
@@ -16,7 +16,6 @@ const Statistics = ({ good, neutral, bad }) => {
 };
 
 const App = () => {
-    // Save clicks of each button to its own state.
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
