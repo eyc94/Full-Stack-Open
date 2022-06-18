f64d88a309e47be432efaf8af61727115e7aeae5
Eric Chin <ericychin94@gmail.com>
2022-06-17 17:13:52 -0700

    Fixed index bug

--- 02. Communicating With Server/notes-frontend/src/App.js
+++ 02. Communicating With Server/notes-frontend/src/App.js
@@ -8,8 +8,8 @@ const App = (props) => {
             <h1>Notes</h1>
             <ul>
                 <li>{notes[0].content}</li>
-                <li>{notes[0].content}</li>
-                <li>{notes[0].content}</li>
+                <li>{notes[1].content}</li>
+                <li>{notes[2].content}</li>
             </ul>
         </div>
     );
