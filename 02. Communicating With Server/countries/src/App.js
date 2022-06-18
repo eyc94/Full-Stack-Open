4e5e311e6e74558858e5b78c994592196ba5423a
Eric Chin <ericychin94@gmail.com>
2022-06-18 16:33:35 -0700

    Added input element and state that holds the input

--- 02. Communicating With Server/countries/src/App.js
+++ 02. Communicating With Server/countries/src/App.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
+    const [countryFilter, setCountryFilter] = useState("");
+
+    const handleFilterChange = (event) => {
+        setCountryFilter(event.target.value);
+    };
 
     return (
         <div>
-            Find Countries <input />
+            Find Countries <input value={countryFilter} onChange={handleFilterChange} />
         </div>
     );
 
