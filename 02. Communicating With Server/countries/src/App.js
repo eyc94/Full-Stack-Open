0fef93c7fc7c21fd1aa875e778fa405a8ab39b94
Eric Chin <ericychin94@gmail.com>
2022-06-18 16:43:16 -0700

    Added axios and used it in useEffect to store countries data in countries state

--- 02. Communicating With Server/countries/src/App.js
+++ 02. Communicating With Server/countries/src/App.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
     const [countryFilter, setCountryFilter] = useState("");
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://restcountries.com/v3.1/all")
+            .then(response => {
+                console.log(response);
+            })
+    }, []);
 
     const handleFilterChange = (event) => {
         setCountryFilter(event.target.value);
