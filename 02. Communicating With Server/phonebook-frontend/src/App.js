9935e7b5fdd19638aae6e6dad95e6e3abb683b43
Eric Chin <ericychin94@gmail.com>
2022-06-17 20:24:59 -0700

    Used the array map method to display the names of phonebook

--- 02. Communicating With Server/phonebook-frontend/src/App.js
+++ 02. Communicating With Server/phonebook-frontend/src/App.js
@@ -18,6 +18,9 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
+            {persons.map(person =>
+                <div key={person.name}>{person.name}</div>
+            )}
         </div>
     );
 };
