85ddbbd587c49b3c9a766b431294f12469a0ac2c
Eric Chin <ericychin94@gmail.com>
2022-06-19 16:16:43 -0700

    Added a button that allows the user to delete a person

--- 02. Communicating With Server/phonebook-frontend/src/components/Persons.js
+++ 02. Communicating With Server/phonebook-frontend/src/components/Persons.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+import personService from "../services/persons";
+
 const Persons = (props) => {
     return (
         <div>
             {props.persons.map(person =>
-                <div key={person.name}>{person.name} {person.number}</div>
+                <div key={person.name}>
+                    {person.name}
+                    {person.number}
+                    <button onClick={() => props.deleteHandler(person.id)}>Delete</button>
+                </div>
             )}
         </div>
     );
