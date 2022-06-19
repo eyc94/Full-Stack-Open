3031ce716d1dfc8527262582048a55bf798e855b
Eric Chin <ericychin94@gmail.com>
2022-06-19 15:26:43 -0700

    Added ability to change a note's importance with the PUT axios method

--- 02. Communicating With Server/notes-frontend/src/App.js
+++ 02. Communicating With Server/notes-frontend/src/App.js
@@ -38,7 +38,13 @@ const App = (props) => {
     };
 
     const toggleImportanceOf = (id) => {
-        console.log(`Importance of ${id} needs to be toggled`);
+        const url = `http://localhost:3001/notes/${id}`;
+        const note = notes.find(n => n.id === id);
+        const changedNote = { ...note, important: !note.important };
+
+        axios.put(url, changedNote).then(response => {
+            setNotes(notes.map(note => note.id !== id ? note : response.data));
+        });
     };
 
     const notesToShow = showAll
