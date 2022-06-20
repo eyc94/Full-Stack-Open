7ae78bfaece1a7ba1f34f0f3e99f493d986e10ec
Eric Chin <ericychin94@gmail.com>
2022-06-19 19:46:51 -0700

    Added space between text and button

--- 02. Communicating With Server/notes-frontend/src/components/Note.js
+++ 02. Communicating With Server/notes-frontend/src/components/Note.js
@@ -6,8 +6,7 @@ const Note = ({ note, toggleImportance }) => {
 
     return (
         <li>
-            {note.content}
-            <button onClick={toggleImportance}>{label}</button>
+            {note.content} <button onClick={toggleImportance}>{label}</button>
         </li>
     );
 };
