93be11febb6f1368762d7363258508b890213770
Eric Chin <ericychin94@gmail.com>
2022-06-19 22:06:46 -0700

    Added success style and style attribute to message

--- 02. Communicating With Server/phonebook-frontend/src/components/Notification.js
+++ 02. Communicating With Server/phonebook-frontend/src/components/Notification.js
@@ -2,12 +2,22 @@ import React from "react";
 
 
 const Notification = ({ message }) => {
+    const successStyle = {
+        color: "green",
+        background: "lightgrey",
+        fontSize: 20,
+        borderStyle: "solid",
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10
+    };
+
     if (message === null) {
         return null;
     }
 
     return (
-        <div>
+        <div style={successStyle}>
             {message}
         </div>
     );
