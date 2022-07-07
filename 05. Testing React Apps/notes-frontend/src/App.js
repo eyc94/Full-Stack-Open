684ba767643c92dcaef7b3ab3fc8ab45dd8ee2a5
Eric Chin <ericychin94@gmail.com>
2022-07-07 03:39:23 -0700

    Display name of user if logged in

--- 05. Testing React Apps/notes-frontend/src/App.js
+++ 05. Testing React Apps/notes-frontend/src/App.js
@@ -127,8 +127,13 @@ const App = () => {
             <h1>Notes</h1>
             <Notification message={errorMessage} />
 
-            {user === null && loginForm()}
-            {user !== null && noteForm()}
+            {user === null ?
+                loginForm() :
+                <div>
+                    <p>{user.name} logged-in</p>
+                    {noteForm()}
+                </div>
+            }
 
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
