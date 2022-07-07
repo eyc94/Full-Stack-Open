c479caae0d6ac52b67bbcc13cf548dc30080b02f
Eric Chin <ericychin94@gmail.com>
2022-07-07 03:44:55 -0700

    Retrieve user if logged in already

--- 05. Testing React Apps/notes-frontend/src/App.js
+++ 05. Testing React Apps/notes-frontend/src/App.js
@@ -23,6 +23,15 @@ const App = () => {
             });
     }, []);
 
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON);
+            setUser(user);
+            noteService.setToken(user.token);
+        }
+    }, []);
+
     const addNote = (event) => {
         event.preventDefault();
 
