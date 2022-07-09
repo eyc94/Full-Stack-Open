cdc577b63f7c654732bab7ca9915acb6301c5b3f
Eric Chin <ericychin94@gmail.com>
2022-07-09 04:03:46 -0700

    Added title

--- 05. Testing React Apps/bloglist-frontend/src/components/BlogForm.js
+++ 05. Testing React Apps/bloglist-frontend/src/components/BlogForm.js
@@ -19,33 +19,36 @@ const BlogForm = ({ createBlog }) => {
     };
 
     return (
-        <form onSubmit={addBlog}>
-            <div>
-                title:
-                <input
-                    type="text"
-                    value={title}
-                    onChange={({ target }) => setTitle(target.value)}
-                />
-            </div>
-            <div>
-                author:
-                <input
-                    type="text"
-                    value={author}
-                    onChange={({ target }) => setAuthor(target.value)}
-                />
-            </div>
-            <div>
-                url:
-                <input
-                    type="text"
-                    value={url}
-                    onChange={({ target }) => setUrl(target.value)}
-                />
-            </div>
-            <button type="submit">Create</button>
-        </form>
+        <div>
+            <h2>Create A New Blog</h2>
+            <form onSubmit={addBlog}>
+                <div>
+                    title:
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={({ target }) => setTitle(target.value)}
+                    />
+                </div>
+                <div>
+                    author:
+                    <input
+                        type="text"
+                        value={author}
+                        onChange={({ target }) => setAuthor(target.value)}
+                    />
+                </div>
+                <div>
+                    url:
+                    <input
+                        type="text"
+                        value={url}
+                        onChange={({ target }) => setUrl(target.value)}
+                    />
+                </div>
+                <button type="submit">Create</button>
+            </form>
+        </div>
     );
 };
 
