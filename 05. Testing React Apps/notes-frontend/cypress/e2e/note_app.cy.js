1e030143fe704ab68f09f45b1a4ccc9fd543a706
Eric Chin <ericychin94@gmail.com>
2022-07-12 02:51:19 -0700

    Reset user and notes before every test

--- 05. Testing React Apps/notes-frontend/cypress/e2e/note_app.cy.js
+++ 05. Testing React Apps/notes-frontend/cypress/e2e/note_app.cy.js
@@ -1,5 +1,12 @@
 describe("Note App", function () {
     beforeEach(function () {
+        cy.request("POST", "http://localhost:3001/api/testing/reset");
+        const user = {
+            name: "Eric",
+            username: "echin",
+            password: "password"
+        };
+        cy.request("POST", "http://localhost:3001/api/users", user);
         cy.visit("http://localhost:3000");
     });
 
