93f7c17f79cefacb02b38e56c619e88e056bd691
Eric Chin <ericychin94@gmail.com>
2022-07-12 02:46:01 -0700

    Added test for creating a new note while logged in

--- 05. Testing React Apps/notes-frontend/cypress/e2e/note_app.cy.js
+++ 05. Testing React Apps/notes-frontend/cypress/e2e/note_app.cy.js
@@ -8,12 +8,30 @@ describe("Note App", function () {
         cy.contains("Note App, EC 2022");
     });
 
-    it("User can login", function () {
+    it("User can log in", function () {
         cy.contains("Login").click();
         cy.get("#username").type("echin");
         cy.get("#password").type("password");
         cy.get("#login-button").click();
 
         cy.contains("Eric logged-in");
+    });
+
+    describe("When logged in", function () {
+        beforeEach(function () {
+            cy.contains("Login").click();
+            cy.get("#username").type("echin");
+            cy.get("#password").type("password");
+            cy.get("#login-button").click();
+
+            cy.contains("Eric logged-in");
+        });
+
+        it("A new note can be created", function () {
+            cy.contains("New Note").click();
+            cy.get("input").type("A note created by cypress");
+            cy.contains("Save").click();
+            cy.contains("A note created by cypress");
+        });
     });
 });
