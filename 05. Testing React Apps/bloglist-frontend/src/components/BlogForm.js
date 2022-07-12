29bc277452ca0969d6023ff0979fadb9d3563759
Eric Chin <ericychin94@gmail.com>
2022-07-12 04:06:37 -0700

    Added id attribute to create blog button

--- 05. Testing React Apps/bloglist-frontend/src/components/BlogForm.js
+++ 05. Testing React Apps/bloglist-frontend/src/components/BlogForm.js
@@ -49,7 +49,7 @@ const BlogForm = ({ createBlog }) => {
                         onChange={({ target }) => setUrl(target.value)}
                     />
                 </div>
-                <button type="submit">Create</button>
+                <button id="create-button" type="submit">Create</button>
             </form>
         </div>
     );
