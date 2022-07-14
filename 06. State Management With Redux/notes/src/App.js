0bebe879e7b8391092f6c9a730670dd9d2be726c
Eric Chin <ericychin94@gmail.com>
2022-07-14 03:17:37 -0700

    Moved noteReducer to a separate module

--- 06. State Management With Redux/notes/src/App.js
+++ 06. State Management With Redux/notes/src/App.js
@@ -1,13 +1,5 @@
 import { createStore } from "redux";
-
-const noteReducer = (state = [], action) => {
-    if (action.type === "NEW_NOTE") {
-        state.concat(action.data);
-        return state;
-    }
-
-    return state;
-};
+import noteReducer from "./reducers/noteReducer";
 
 const store = createStore(noteReducer);
 
