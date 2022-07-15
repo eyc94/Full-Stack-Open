72442460d419cff7a05f5e8d97fde7bb02f9d829
Eric Chin <ericychin94@gmail.com>
2022-07-15 00:59:32 -0700

    Added changes for uncontrolled forms and action creators

--- 06. State Management With Redux/notes/src/App.js
+++ 06. State Management With Redux/notes/src/App.js
@@ -3,30 +3,47 @@ import noteReducer from "./reducers/noteReducer";
 
 const store = createStore(noteReducer);
 
-store.dispatch({
-    type: "NEW_NOTE",
-    data: {
-        content: "The app state is in redux store",
-        important: true,
-        id: 1
-    }
-});
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
-store.dispatch({
-    type: "NEW_NOTE",
-    data: {
-        content: "State changes are made with actions",
-        important: false,
-        id: 2
-    }
-});
+const createNote = (content) => {
+    return {
+        type: "NEW_NOTE",
+        data: {
+            content,
+            important: false,
+            id: generateId()
+        }
+    };
+};
+
+const toggleImportanceOf = (id) => {
+    return {
+        type: "TOGGLE_IMPORTANCE",
+        data: { id }
+    };
+};
 
 const App = () => {
+    const addNote = (event) => {
+        event.preventDefault();
+        const content = event.target.note.value;
+        event.target.note.value = "";
+        store.dispatch(createNote(content));
+    };
+
+    const toggleImportance = (id) => {
+        store.dispatch(toggleImportanceOf(id));
+    };
+
     return (
         <div>
+            <form onSubmit={addNote}>
+                <input name="note" />
+                <button type="submit">Add</button>
+            </form>
             <ul>
                 {store.getState().map(note =>
-                    <li key={note.id}>
+                    <li key={note.id} onClick={() => toggleImportance(note.id)}>
                         {note.content} <strong>{note.important ? "important" : ""}</strong>
                     </li>
                 )}
