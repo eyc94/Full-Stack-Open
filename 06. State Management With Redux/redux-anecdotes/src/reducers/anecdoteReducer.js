adc54a30aa3091851da7da1192651c839539ba4b
Eric Chin <ericychin94@gmail.com>
2022-07-15 02:08:55 -0700

    Created createAnecdote action creator

--- 06. State Management With Redux/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ 06. State Management With Redux/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -39,4 +39,11 @@ const reducer = (state = initialState, action) => {
     };
 };
 
+export const createAnecdote = (content) => {
+    return {
+        type: "NEW_ANECDOTE",
+        data: content
+    };
+};
+
 export default reducer;
