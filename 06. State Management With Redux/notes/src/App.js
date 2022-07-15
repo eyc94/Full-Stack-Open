672191a81870f952b25eada32dcd1f0c2a02d76e
Eric Chin <ericychin94@gmail.com>
2022-07-15 01:03:47 -0700

    Refactored App

--- 06. State Management With Redux/notes/src/App.js
+++ 06. State Management With Redux/notes/src/App.js
@@ -1,38 +1,19 @@
-import { createStore } from "redux";
-import noteReducer from "./reducers/noteReducer";
-
-const store = createStore(noteReducer);
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
-
-const createNote = (content) => {
-    return {
-        type: "NEW_NOTE",
-        data: {
-            content,
-            important: false,
-            id: generateId()
-        }
-    };
-};
-
-const toggleImportanceOf = (id) => {
-    return {
-        type: "TOGGLE_IMPORTANCE",
-        data: { id }
-    };
-};
+import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
+    const dispatch = useDispatch();
+    const notes = useSelector(state => state);
+
     const addNote = (event) => {
         event.preventDefault();
         const content = event.target.note.value;
         event.target.note.value = "";
-        store.dispatch(createNote(content));
+        dispatch(createNote(content));
     };
 
     const toggleImportance = (id) => {
-        store.dispatch(toggleImportanceOf(id));
+        dispatch(toggleImportanceOf(id));
     };
 
     return (
@@ -42,7 +23,7 @@ const App = () => {
                 <button type="submit">Add</button>
             </form>
             <ul>
-                {store.getState().map(note =>
+                {notes.map(note =>
                     <li key={note.id} onClick={() => toggleImportance(note.id)}>
                         {note.content} <strong>{note.important ? "important" : ""}</strong>
                     </li>
