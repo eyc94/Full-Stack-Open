e8d117818f4e26d492d846d56684eeb35425f490
Eric Chin <ericychin94@gmail.com>
2022-07-15 13:03:13 -0700

    Dispatched actions to see its results in console

--- 06. State Management With Redux/notes/src/index.js
+++ 06. State Management With Redux/notes/src/index.js
@@ -7,6 +7,9 @@ import App from "./App";
 import noteReducer from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
 
+import { createNote } from "./reducers/noteReducer";
+import { filterChange } from "./reducers/filterReducer";
+
 const reducer = combineReducers({
     notes: noteReducer,
     filter: filterReducer
@@ -15,6 +18,10 @@ const reducer = combineReducers({
 const store = createStore(reducer);
 
 console.log(store.getState());
+
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(filterChange("IMPORTANT"));
+store.dispatch(createNote("combineReducers forms one reducer from many simple reducers"));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     // <Provider store={store}>
