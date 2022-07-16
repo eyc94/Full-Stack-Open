97522565fabb1741c8ff2a0ac1de2426a0d05c8c
Eric Chin <ericychin94@gmail.com>
2022-07-16 02:28:57 -0700

    Fixed error using wrong variable

--- 06. State Management With Redux/notes/src/components/NewNote.js
+++ 06. State Management With Redux/notes/src/components/NewNote.js
@@ -10,7 +10,7 @@ const NewNote = (props) => {
         const content = event.target.note.value;
         event.target.note.value = "";
         const newNote = await noteService.createNew(content);
-        dispatch(createNote(content));
+        dispatch(createNote(newNote));
     };
 
     return (
