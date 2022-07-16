d037955eba93bd5481dd0f3dcb7217863888e34c
Eric Chin <ericychin94@gmail.com>
2022-07-16 16:30:24 -0700

    Removed unused modules

--- 06. State Management With Redux/notes/src/App.js
+++ 06. State Management With Redux/notes/src/App.js
@@ -2,8 +2,6 @@ import { useEffect } from "react";
 import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
 import VisibilityFilter from "./components/VisibilityFilter";
-import noteService from "./services/notes";
-import { setNotes } from "./reducers/noteReducer";
 import { useDispatch } from "react-redux";
 import { initializeNotes } from "./reducers/noteReducer";
 
