45889fea3610494fa866ad7d679afdb04d47ac5f
Eric Chin <ericychin94@gmail.com>
2022-07-16 02:23:08 -0700

    Removed unused statements

--- 06. State Management With Redux/notes/src/index.js
+++ 06. State Management With Redux/notes/src/index.js
@@ -4,10 +4,8 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import App from "./App";
 
-import noteReducer, { setNotes } from "./reducers/noteReducer";
+import noteReducer from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
-
-import noteService from "./services/notes";
 
 const store = configureStore({
     reducer: {
