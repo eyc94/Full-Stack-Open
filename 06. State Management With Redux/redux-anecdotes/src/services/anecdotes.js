296879e77b7eecd41e8fa3f03f64d9618d7b6f1f
Eric Chin <ericychin94@gmail.com>
2022-07-16 04:21:43 -0700

    Added createNew axios function to handle creating new anecdotes

--- 06. State Management With Redux/redux-anecdotes/src/services/anecdotes.js
+++ 06. State Management With Redux/redux-anecdotes/src/services/anecdotes.js
@@ -7,4 +7,11 @@ const getAll = async () => {
     return response.data;
 };
 
-export default { getAll };
+const createNew = async (content) => {
+    const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+    const object = { content, id: generateId(), votes: 0 };
+    const response = await axios.post(baseUrl, object);
+    return response.data;
+};
+
+export default { getAll, createNew };
