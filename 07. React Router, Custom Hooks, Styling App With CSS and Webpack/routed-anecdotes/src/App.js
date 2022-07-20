911346cd4bb54cd61ca36fc027220677021a7342
Eric Chin <ericychin94@gmail.com>
2022-07-19 18:02:19 -0700

    Ability to click on anecdote from list and view its details via an Anecdote component

--- 07. React Router, Custom Hooks, Styling App With CSS and Webpack/routed-anecdotes/src/App.js
+++ 07. React Router, Custom Hooks, Styling App With CSS and Webpack/routed-anecdotes/src/App.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import {
     BrowserRouter as Router,
-    Routes, Route, Link
+    Routes, Route, Link,
+    useParams
 } from "react-router-dom";
 
 const Menu = () => {
@@ -18,13 +19,26 @@ const Menu = () => {
     );
 };
 
+const Anecdote = ({ anecdotes }) => {
+    const id = useParams().id;
+    const anecdote = anecdotes.find(a => a.id === Number(id));
+    return (
+        <div>
+            <h2>{anecdote.content}</h2>
+            <div>Author: {anecdote.author}</div>
+            <div>Info: {anecdote.info}</div>
+            <div>Votes: {anecdote.votes}</div>
+        </div>
+    );
+};
+
 const AnecdoteList = ({ anecdotes }) => (
     <div>
         <h2>Anecdotes</h2>
         <ul>
             {anecdotes.map(anecdote =>
                 <li key={anecdote.id}>
-                    {anecdote.content}
+                    <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
                 </li>
             )}
         </ul>
@@ -132,6 +146,7 @@ const App = () => {
                 <Menu />
 
                 <Routes>
+                    <Route path="/anecdotes/:id" element={<Anecdote anecdotes={anecdotes} />} />
                     <Route path="/create" element={<CreateNew addNew={addNew} />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
