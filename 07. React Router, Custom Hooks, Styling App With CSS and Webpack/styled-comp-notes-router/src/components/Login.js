175245fdd61fddf09c9f001c46ef2ea4ce6da688
Eric Chin <ericychin94@gmail.com>
2022-07-21 03:48:58 -0700

    Added style to login form via styled comp

--- 07. React Router, Custom Hooks, Styling App With CSS and Webpack/styled-comp-notes-router/src/components/Login.js
+++ 07. React Router, Custom Hooks, Styling App With CSS and Webpack/styled-comp-notes-router/src/components/Login.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { TextField, Button } from "@mui/material";
+import styled from "styled-components";
+
+const Button = styled.button`
+    background: Bisque;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid Chocolate;
+    border-radius: 3px;
+`;
+
+const Input = styled.input`
+    margin: 0.25em;
+`;
 
 const Login = (props) => {
     const navigate = useNavigate();
@@ -16,15 +29,15 @@ const Login = (props) => {
             <h2>Login</h2>
             <form onSubmit={onSubmit}>
                 <div>
-                    <TextField label="Username" />
+                    Username:
+                    <Input />
                 </div>
                 <div>
-                    <TextField label="Password" type="password" />
+                    Password:
+                    <Input />
                 </div>
                 <div>
-                    <Button variant="contained" color="primary" type="submit">
-                        Login
-                    </Button>
+                    <Button type="submit" primary="">Login</Button>
                 </div>
             </form>
         </div>
