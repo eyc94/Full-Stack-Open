85c9cc8bed4e2861f9efef712b617232141a9bde
Eric Chin <ericychin94@gmail.com>
2022-07-21 03:17:02 -0700

    Added login form style

--- 07. React Router, Custom Hooks, Styling App With CSS and Webpack/notes-router/src/components/Login.js
+++ 07. React Router, Custom Hooks, Styling App With CSS and Webpack/notes-router/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Table, Form, Button } from "react-bootstrap";
 
 const Login = (props) => {
     const navigate = useNavigate();
@@ -13,15 +14,15 @@ const Login = (props) => {
     return (
         <div>
             <h2>Login</h2>
-            <form onSubmit={onSubmit}>
-                <div>
-                    Username: <input />
-                </div>
-                <div>
-                    Password: <input type="password" />
-                </div>
-                <button type="submit">Login</button>
-            </form>
+            <Form onSubmit={onSubmit}>
+                <Form.Group>
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control type="text" name="username" />
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control type="password" />
+                    <Button variant="primary" type="submit">Login</Button>
+                </Form.Group>
+            </Form>
         </div>
     );
 };
