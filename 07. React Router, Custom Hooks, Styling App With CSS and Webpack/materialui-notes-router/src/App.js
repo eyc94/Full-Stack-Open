6fd5e670143773648f7a6752c79b911dab23ae19
Eric Chin <ericychin94@gmail.com>
2022-07-21 03:31:49 -0700

    Added app return in Container wrapper

--- 07. React Router, Custom Hooks, Styling App With CSS and Webpack/materialui-notes-router/src/App.js
+++ 07. React Router, Custom Hooks, Styling App With CSS and Webpack/materialui-notes-router/src/App.js
@@ -9,7 +9,7 @@ import Note from "./components/Note";
 import Notes from "./components/Notes";
 import Users from "./components/Users";
 import Login from "./components/Login";
-import { Alert, Nav, Navbar } from "react-bootstrap";
+import { Container } from "@mui/material";
 
 const App = () => {
     const [notes, setNotes] = useState([
@@ -54,49 +54,50 @@ const App = () => {
         : null;
 
     return (
-        <div className="container">
-            {(message &&
-                <Alert variant="success">
-                    {message}
-                </Alert>
-            )}
+        <Container>
+            <div className="container">
+                {(message &&
+                    <Alert variant="success">
+                        {message}
+                    </Alert>
+                )}
 
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#" as="span">
-                            <Link style={padding} to="/">Home</Link>
-                        </Nav.Link>
-                        <Nav.Link href="#" as="span">
-                            <Link style={padding} to="/notes">Notes</Link>
-                        </Nav.Link>
-                        <Nav.Link href="#" as="span">
-                            <Link style={padding} to="/users">Users</Link>
-                        </Nav.Link>
-                        <Nav.Link href="#" as="span">
-                            {user
-                                ? <em style={padding}>{user} loggged in</em>
-                                : <Link style={padding} to="/login">Login</Link>
-                            }
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#" as="span">
+                                <Link style={padding} to="/">Home</Link>
+                            </Nav.Link>
+                            <Nav.Link href="#" as="span">
+                                <Link style={padding} to="/notes">Notes</Link>
+                            </Nav.Link>
+                            <Nav.Link href="#" as="span">
+                                <Link style={padding} to="/users">Users</Link>
+                            </Nav.Link>
+                            <Nav.Link href="#" as="span">
+                                {user
+                                    ? <em style={padding}>{user} loggged in</em>
+                                    : <Link style={padding} to="/login">Login</Link>
+                                }
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
 
-            <Routes>
-                <Route path="/notes/:id" element={<Note note={note} />} />
-                <Route path="/notes" element={<Notes notes={notes} />} />
-                <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
-                <Route path="/login" element={<Login onLogin={login} />} />
-                <Route path="/" element={<Home />} />
-            </Routes>
+                <Routes>
+                    <Route path="/notes/:id" element={<Note note={note} />} />
+                    <Route path="/notes" element={<Notes notes={notes} />} />
+                    <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
+                    <Route path="/login" element={<Login onLogin={login} />} />
+                    <Route path="/" element={<Home />} />
+                </Routes>
 
-            <div>
-                <em>EC Notes App, 2022</em>
+                <div>
+                    <em>EC Notes App, 2022</em>
+                </div>
             </div>
-        </div>
-
+        </Container>
     );
 };
 
