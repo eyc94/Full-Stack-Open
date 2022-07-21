d5c6fa061c2cd95b01ed2dc4892e263f39541bc1
Eric Chin <ericychin94@gmail.com>
2022-07-21 02:52:53 -0700

    Moved Users to new file

--- 07. React Router, Custom Hooks, Styling App With CSS and Webpack/notes-router/src/index.js
+++ 07. React Router, Custom Hooks, Styling App With CSS and Webpack/notes-router/src/index.js
@@ -10,19 +10,7 @@ import {
 } from "react-router-dom";
 import Home from "./components/Home";
 import Note from "./components/Note";
-
-const Notes = ({ notes }) => (
-    <div>
-        <h2>Notes</h2>
-        <ul>
-            {notes.map(note =>
-                <li key={note.id}>
-                    <Link to={`/notes/${note.id}`}>{note.content}</Link>
-                </li>
-            )}
-        </ul>
-    </div>
-);
+import Notes from "./components/Notes";
 
 const Users = () => (
     <div>
